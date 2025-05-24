6645ecb13aa89fcda11a06cf57067ce52d45cb02
Shawn <shawn.rushefsky@gmail.com>
2025-05-24 10:22:59 -0400

    better logging

--- src/autoscale.ts
+++ src/autoscale.ts
@@ -21,8 +21,14 @@ export async function evaluateScalingRule(env: Env, rule: DBScalingRule) {
 		env
 	);
 	const constrainedNumJobs = Math.min(rule.max_replicas, Math.max(rule.min_replicas, numJobs));
-
-	await setReplicasForContainerGroup(env, constrainedNumJobs, rule.org_name, rule.project_name, rule.container_group_name);
+	try {
+		console.log(
+			`Evaluating scaling rule ${rule.container_group_id} for container group ${rule.org_name}/${rule.project_name}/${rule.container_group_name}: ${numJobs} jobs, constrained to ${constrainedNumJobs} replicas`
+		);
+		await setReplicasForContainerGroup(env, constrainedNumJobs, rule.org_name, rule.project_name, rule.container_group_name);
+	} catch (error) {
+		console.error(`Error evaluating scaling rule ${rule.container_group_id}: ${error}`);
+	}
 }
 
 export async function setReplicasForContainerGroup(
@@ -34,20 +40,20 @@ export async function setReplicasForContainerGroup(
 ) {
 	const group = await getContainerGroupByName(env, orgName, projectName, containerGroupName);
 	if (!group) {
-		console.log('Container group not found');
+		console.log(`Container group not found: ${orgName}/${projectName}/${containerGroupName}`);
 		return;
 	}
 
-	console.log(`Setting replicas for container group ${containerGroupName} to ${numReplicas}`);
+	console.log(`Setting replicas for container group ${orgName}/${projectName}/${containerGroupName} to ${numReplicas}`);
 
 	if (numReplicas === 0 && group.current_state.status === 'running') {
-		console.log(`Stopping container group ${containerGroupName}`);
+		console.log(`Stopping container group ${orgName}/${projectName}/${containerGroupName}`);
 		await stopContainerGroup(env, orgName, projectName, containerGroupName);
 		return;
 	} else if (numReplicas === 0) {
 		return;
 	} else if (numReplicas > 0 && group.current_state.status === 'stopped') {
-		console.log(`Starting container group ${containerGroupName}`);
+		console.log(`Starting container group ${orgName}/${projectName}/${containerGroupName}`);
 		await startContainerGroup(env, orgName, projectName, containerGroupName);
 	}
 
